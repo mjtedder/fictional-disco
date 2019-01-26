6bba84ca8b2cfe041533f4f44f08facba096a3cc
mjtedder <mjtedder@gmail.com>
2019-01-26 09:28:53 -0700

    Add pseudocode

---
+++ 10-nodejs/01-Activities/07-Multiples/Unsolved/multiple.js
@@ -0,0 +1,11 @@
+// Take in an input value in the form of an integer
+
+// Go from 6 to the number. Adding up 6 each time (to only get multiples of 6).
+
+      // Each time add i to the sum. (ex: 6, 12, 18, etc.)
+
+// Print out the sum
+
+// Example:
+// User Input: node multiple 12
+// Output: 18 (6 + 12 = 18)
