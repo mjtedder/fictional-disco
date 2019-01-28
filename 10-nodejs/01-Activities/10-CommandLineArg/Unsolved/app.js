89e757102b4482ea642613031154da3636a48c39
mjtedder <mjtedder@gmail.com>
2019-01-28 14:58:26 -0700

    Refactor solution

--- 10-nodejs/01-Activities/10-CommandLineArg/Unsolved/app.js
+++ 10-nodejs/01-Activities/10-CommandLineArg/Unsolved/app.js
@@ -1,8 +1,14 @@
-var arg1 = parseFloat(process.argv[2])
+/* var arg1 = parseFloat(process.argv[2])
 var arg2 = parseFloat(process.argv[3])
 
 if (arg1 === arg2) {
     console.log('True')
 } else {
     console.log('False')
-}
+} */
+
+// Refactored solution
+console.log(process.argv[2] === process.argv[3])
+
+// Check if two arguments are multiples of 7
+console.log((process.argv[2] % 7) + (process.argv[3] % 7) === 0)
