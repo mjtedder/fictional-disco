2c3fc6a1aaa9e61f1d95bf32612246dba0341aa3
mjtedder <mjtedder@gmail.com>
2019-01-30 12:40:58 -0700

    Add a variable that allows user to search a movie of their choice form the CLI

--- 10-nodejs/01-Activities/17-OMDB_Axios/omdbAxios.js
+++ 10-nodejs/01-Activities/17-OMDB_Axios/omdbAxios.js
@@ -1,8 +1,9 @@
 // Include the axios npm package (Don't forget to run "npm install axios" in this folder first!)
 var axios = require("axios");
+var movie = process.argv[2]
 
 // Then run a request with axios to the OMDB API with the movie specified
-axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy").then(
+axios.get('http://www.omdbapi.com/?t=' + movie + '&plot=short&apikey=trilogy').then(
   function(response) {
     console.log("The movie's rating is: " + response.data.imdbRating);
   }
