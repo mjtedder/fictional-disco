ffe1e297706d2db47d8e19eb386aeba1263301a0
mjtedder <mjtedder@gmail.com>
2019-01-30 15:22:21 -0700

    Sorting numbers using ES6 arrow function

--- 10-nodejs/01-Activities/20-CommandSort/Unsolved/commandSortUnsolved.js
+++ 10-nodejs/01-Activities/20-CommandSort/Unsolved/commandSortUnsolved.js
@@ -2,3 +2,9 @@
 // Create an application that takes in a series of numbers then sorts them.
 // Feel encouraged to use Stack or Google to find the "sort" code.
 // ===========================================================================================
+
+const numbers = process.argv.slice(2)
+
+numbers.sort((a, b) => a - b)
+
+console.log(numbers)
