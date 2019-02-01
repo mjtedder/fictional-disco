7707092bff16d20c4515355d6b0875a9c3c15cfb
mjtedder <mjtedder@gmail.com>
2019-01-31 18:05:24 -0700

    Add Geocoder dependency

--- 10-nodejs/01-Activities/23-GeocodeNPM/Unsolved/geocode.js
+++ 10-nodejs/01-Activities/23-GeocodeNPM/Unsolved/geocode.js
@@ -5,22 +5,31 @@
 // Easier: User will provide the city and state in the following format: "Atlanta, GA", "Houston, TX"
 // Slightly More Challenging: User will provide the address in any format: "151 Sip Ave Jersey City, NJ", "Austin, TX", etc.
 
-// All: Be sure to console log the output using JSON.stringify in "pretty-print" format. 
+// All: Be sure to console log the output using JSON.stringify in "pretty-print" format.
 
 // ========================================================================================================================
 
 // Include the geocoder NPM package (Remember to run "npm install geocoder"!)
+const NodeGeocoder = require('node-geocoder')
 
+const options = {
+    provider: 'mapquest',
+    apiKey: 'GAN2yIJEAC5Iam9YHtLy4AuU4SDA67Ps    '
+}
 
-
+const geocoder = NodeGeocoder(options)
 
 // Take in the command line arguments
-
-
-
+const city = process.argv[2]
+const state = process.argv[3]
+const address = city + state
 
 // Build your address as an array or string
-
+// Geocoding
+geocoder.geocode(address, function ( err, res ) {
+    if (err) throw err
+    console.log(JSON.stringify(res, null, 2));
+  });
 
 
 
