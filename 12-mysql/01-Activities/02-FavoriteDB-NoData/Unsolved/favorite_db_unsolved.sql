dd6915084c3658ec3f8a317e11a30dbe72b7c57c
mjtedder <mjtedder@gmail.com>
2019-02-09 13:38:14 -0700

    Insert 3 rows of data into the 3 tables created

--- 12-mysql/01-Activities/02-FavoriteDB-NoData/Unsolved/favorite_db_unsolved.sql
+++ 12-mysql/01-Activities/02-FavoriteDB-NoData/Unsolved/favorite_db_unsolved.sql
@@ -24,7 +24,7 @@ CREATE TABLE favorite_songs (
 
 CREATE TABLE favorite_movies (
   -- Create a numeric column called "id" which automatically increments and cannot be null --
-  ID INTEGER NOT NULL AUTO_INCREMENT,
+  id INTEGER NOT NULL AUTO_INCREMENT,
   -- Create a string column called "movie" which cannot be null --
   movie VARCHAR(30) NOT NULL,
   -- Create a boolean column called "five_times" that sets the default value to false if nothing is entered --
@@ -32,5 +32,17 @@ CREATE TABLE favorite_movies (
   -- Make an integer column called "score" --
   score INTEGER(10),
   -- Set the primary key of the table to id --
-  PRIMARY KEY
+  PRIMARY KEY id
 );
+
+INSERT INTO favorite_foods (food, score)
+  VALUES ("Pizza", 10), ("Kimchi", 8), ("Curry", 6);
+
+INSERT INTO favorite_songs (song, artist, score)
+  VALUES ("August", "Umphrey's Mcgee", 9), ("Strange Times", "Aqueous", 9), ("Vactrollio", "Papadosio", 10);
+
+INSERT INTO favorite_movies (movie, five_times, score)
+  VALUES ("Pulp Fiction", true, 10), ("The Matrix", true, 9), ("Eternal", true, 8);
+
+-- Display favorite_movies table --
+SELECT * FROM favorite_movies
