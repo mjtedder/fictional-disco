f93a056a084be4ef33d181b68f46d1b4d3636f1e
mjtedder <mjtedder@gmail.com>
2019-02-12 16:14:16 -0700

    Full CRUD capability

--- 12-mysql/01-Activities/08-playlistRead/Unsolved/playlistConnect.js
+++ 12-mysql/01-Activities/08-playlistRead/Unsolved/playlistConnect.js
@@ -14,25 +14,55 @@ connection.connect(function (err) {
         return
     }
     console.log('connected as id ' + connection.threadId)
-    readPlaylist();
-    readProgGenre();
+    createEntry();
 })
 
+// CRUD - Create
+function createEntry() {
+    var post = {title: 'Got A Girl', artist: 'The Black Seeds', genre: 'Reggae'};
+    var query = connection.query('INSERT INTO playlist SET?', post, function(err, res, fields) {
+        if (err) throw err;
+    })
+    console.log(query.sql)
+    updateSong();
+}
+
+// CRUD - Read
 function readPlaylist() {
-    console.log('\nCurrent Playlist Contents:\n')
     connection.query('SELECT * FROM playlist', function (err, res) {
         for (var i = 0; i < res.length; i++) {
             console.log(res[i].id + ' | ' + res[i].title + ' | ' + res[i].artist + ' | ' + res[i].genre);
         }
         console.log('------------------------------------------------------------------------------------');
     });
+    connection.end();
 }
 
 function readProgGenre() {
-    console.log('\nAll Prog Rock Songs:\n')
-    connection.query('SELECT * FROM playlist WHERE genre = ?', ['Prog Rock'], function(err, res) {
-        if (err) throw err;
-        console.log(res);
-    })
+    var query = connection.query('SELECT * FROM playlist WHERE genre = ?', ['Prog Rock'], function (err, res) {
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].id + ' | ' + res[i].title + ' | ' + res[i].artist + ' | ' + res[i].genre);
+        }
+        console.log('------------------------------------------------------------------------------------');
+    });
+    console.log(query.sql);
     connection.end();
+}
+
+// CRUD - Update
+function updateSong() {
+    var query = connection.query('UPDATE playlist SET title = ? WHERE id = ?', ['Skyway', 2],
+    function (err, res) {
+        console.log(res.affectedRows + ' Updated song!');
+    })
+    deleteSong();
+}
+
+// CRUD - Delete
+function deleteSong() {
+    var query = connection.query('DELETE FROM playlist WHERE ?', {title: 'Got A Girl'},
+    function (err, res) {
+        console.log(res.affectedRows + ' Deleted song!');
+    })
+    readPlaylist();
 }
