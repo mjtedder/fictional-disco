4647536c6b1a79cba53d283b80116c74d6e67ddf
mjtedder <mjtedder@gmail.com>
2019-02-12 12:28:08 -0700

    Connecting to DB using secure credentials with dotenv

--- 12-mysql/01-Activities/08-playlistRead/Unsolved/playlistConnect.js
+++ 12-mysql/01-Activities/08-playlistRead/Unsolved/playlistConnect.js
@@ -1,9 +1,10 @@
-var mysql = require('mysql');
+var mysql = require('mysql')
+require('dotenv').config()
 
 var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
     // database: 'playlist_db'
 })
 
