e26a4834dd0e404e8683c7f6df294550b1619aa1
mjtedder <mjtedder@gmail.com>
2019-02-12 14:03:17 -0700

    Add function to query db for only prog rock songs

--- 12-mysql/01-Activities/08-playlistRead/Unsolved/playlistConnect.js
+++ 12-mysql/01-Activities/08-playlistRead/Unsolved/playlistConnect.js
@@ -5,16 +5,34 @@ var connection = mysql.createConnection({
     host: process.env.HOST,
     user: process.env.USER,
     password: process.env.PASSWORD,
-    // database: 'playlist_db'
+    database: process.env.DATABASE
 })
 
-connection.connect(function(err) {
-    if(err) {
+connection.connect(function (err) {
+    if (err) {
         console.error('error connecting: ' + err.stack)
         return
     }
     console.log('connected as id ' + connection.threadId)
-    connection.end()
+    readPlaylist();
+    readProgGenre();
 })
 
+function readPlaylist() {
+    console.log('\nCurrent Playlist Contents:\n')
+    connection.query('SELECT * FROM playlist', function (err, res) {
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].id + ' | ' + res[i].title + ' | ' + res[i].artist + ' | ' + res[i].genre);
+        }
+        console.log('------------------------------------------------------------------------------------');
+    });
+}
 
+function readProgGenre() {
+    console.log('\nAll Prog Rock Songs:\n')
+    connection.query('SELECT * FROM playlist WHERE genre = ?', ['Prog Rock'], function(err, res) {
+        if (err) throw err;
+        console.log(res);
+    })
+    connection.end();
+}
