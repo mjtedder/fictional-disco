b7377b6ac1c810d43dc89afb2d18960b1be47ec0
mjtedder <mjtedder@gmail.com>
2019-02-13 09:05:36 -0700

    Post bid function inserting into database.

--- 12-mysql/01-Activities/10-GreatBay/Unsolved/greatBay.js
+++ 12-mysql/01-Activities/10-GreatBay/Unsolved/greatBay.js
@@ -1,7 +1,7 @@
 // Required dependencies
 var mysql = require('mysql')
 var inquirer = require('inquirer')
-require ('dotenv').config()
+require('dotenv').config()
 
 // Connect to db
 var connection = mysql.createConnection({
@@ -12,9 +12,65 @@ var connection = mysql.createConnection({
     database: process.env.database
 })
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
-    console.log('connected as id ' + connection.threadId)
+    //console.log('connected as id ' + connection.threadId)
+    start();
 })
 
 // start application using inquirer
+function start() {
+    inquirer.prompt({
+            type: 'list',
+            name: 'choice',
+            message: 'Please choose an option.',
+            choices: ['POST AN ITEM', 'BID ON AN ITEM', 'EXIT']
+        })
+        .then(function (answers) {
+            if (answers.choice === 'POST AN ITEM') {
+                postBid();
+            } else {
+                makeBid();
+            }
+        })
+}
+
+// post bid
+function postBid() {
+    inquirer.prompt([{
+                type: 'input',
+                name: 'item',
+                message: 'What is the item you would like to submit?',
+            },
+            {
+                type: 'input',
+                name: 'category',
+                message: 'What category would you like to place your auction?'
+            },
+            {
+                type: 'input',
+                name: 'startBid',
+                message: 'What would you like your starting bid to be?',
+                valid: function (value) {
+                    if (isNaN(value) === false) {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+        ])
+        .then(function (answer) {
+                connection.query(
+                    'INSERT INTO auctions SET ?', {
+                        item_name: answer.item,
+                        category: answer.category,
+                        starting_bid: answer.startBid,
+                        highest_bid: answer.startBid
+                    },
+                    function (err) {
+                        if(err) throw err;
+                        console.log('Your item has been submitted for bidding!');
+                    })
+            })
+        }
+        start();
